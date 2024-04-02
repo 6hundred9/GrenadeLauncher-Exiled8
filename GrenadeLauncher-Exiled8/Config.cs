c9faa55630753c59b9eb6434f5c9ad5fc440047b
Mert Ali Bayram <mertali.bayram01@dogaokullari.k12.tr>
2024-04-02 22:22:10 +0300

    whar

--- GrenadeLauncher-Exiled8/Config.cs
+++ GrenadeLauncher-Exiled8/Config.cs
@@ -10,6 +10,5 @@ namespace GrenadeLauncher_Exiled8
         public bool Debug { get; set; } = false;
         
         public GrenadeLauncher grenadeLauncher { get; set; } = new GrenadeLauncher();
-        
     }
 }
