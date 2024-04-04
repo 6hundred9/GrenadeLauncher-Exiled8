1c77b10b0d7656143982b7e89a76dda2e10cb5fe
Mert Ali Bayram <mertali.bayram01@dogaokullari.k12.tr>
2024-04-04 18:17:40 +0300

    update

--- GrenadeLauncher-Exiled8/Plugin.cs
+++ GrenadeLauncher-Exiled8/Plugin.cs
@@ -10,7 +10,7 @@ namespace GrenadeLauncher_Exiled8
         public override string Author { get; } = "6hundred9";
         public override string Name { get; } = "Grenade Launcher";
         public override string Prefix { get; } = "grnLauncher";
-        public override Version RequiredExiledVersion { get; } = new Version(8, 8, 1);
+        public override Version RequiredExiledVersion { get; } = new Version(8, 8, 0);
 
         public override void OnEnabled()
         {
