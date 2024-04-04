20b00b424b88aa73f782da76f831101defa6d501
Mert Ali Bayram <mertali.bayram01@dogaokullari.k12.tr>
2024-04-04 20:22:24 +0300

    shotgun

--- GrenadeLauncher-Exiled8/Plugin.cs
+++ GrenadeLauncher-Exiled8/Plugin.cs
@@ -15,7 +15,7 @@ namespace GrenadeLauncher_Exiled8
         public override void OnEnabled()
         {
             Instance = this;
-            Config.grenadeLauncher.Type = ItemType.GunRevolver;
+            Config.grenadeLauncher.Type = ItemType.GunShotgun;
             Config.grenadeLauncher.Register();
             base.OnEnabled();
             
